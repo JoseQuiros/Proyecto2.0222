829c69f36d46dab7b2c59432c9b30be4e7d316a6
Andrew <Andrew@10.140.136.107>
2018-06-21 16:34:22 -0600

    login

--- src/GUI/menuLogin.java
+++ src/GUI/menuLogin.java
@@ -7,6 +7,7 @@
 package GUI;
 import USUARIOS.validacionDatos;
 import USUARIOS.validacionDatosUsuario;
+import vehiculo.bus;
 
 /**
  *
@@ -36,6 +37,8 @@ public class menuLogin extends javax.swing.JFrame {
         usuarioTextField = new javax.swing.JTextField();
         contraseñaTextField = new javax.swing.JPasswordField();
         iniciarSesion = new javax.swing.JButton();
+        botonRegistro = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
         ImagenFondoLogin = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -90,7 +93,26 @@ public class menuLogin extends javax.swing.JFrame {
                 iniciarSesionActionPerformed(evt);
             }
         });
-        getContentPane().add(iniciarSesion, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 290, -1, 30));
+        getContentPane().add(iniciarSesion, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 300, -1, 30));
+
+        botonRegistro.setBackground(new java.awt.Color(0, 0, 0));
+        botonRegistro.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        botonRegistro.setForeground(new java.awt.Color(255, 255, 255));
+        botonRegistro.setText("Registrarse");
+        botonRegistro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonRegistroActionPerformed(evt);
+            }
+        });
+        getContentPane().add(botonRegistro, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 300, -1, 30));
+
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 50, -1, -1));
 
         ImagenFondoLogin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/fondo1.jpg"))); // NOI18N
         getContentPane().add(ImagenFondoLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 410, 420));
@@ -116,11 +138,22 @@ public class menuLogin extends javax.swing.JFrame {
         validacionDatos validacion=new validacionDatos();
         String nombreUsuario=usuarioTextField.getText();
         String contraseña=contraseñaTextField.getText();
-        
+        if(nombreUsuario.equalsIgnoreCase("rolo")){
+            if (contraseña.equals("programacion")) {
+                gerenteGUI gerente= new gerenteGUI();
+                gerente.setVisible(true);
+                gerente.setLocationRelativeTo(null);
+                this.dispose();
+            }
+        }
         if(validacion.validarUsuario(nombreUsuario,"colaborador.txt")==true){
             System.out.println("usuario existe");
             if (validacion.validarContraseña(contraseña, "colaborador.txt")==true) {
                 System.out.println("contraseña correcta");
+                colaboradorGUI cola= new colaboradorGUI();
+                cola.setVisible(true);
+                cola.setLocationRelativeTo(null);
+                this.dispose();
             }else{
                 System.out.println("EL usuario existe pero la contraseña es incorrecta");
             }
@@ -133,6 +166,7 @@ public class menuLogin extends javax.swing.JFrame {
             System.out.println("usuario existe");
             if (validacionUsuario.validarContraseña(contraseña, "Clientes.txt")==true) {
                 System.out.println("contraseña correcta");
+                this.dispose();
             }else{
                 System.out.println("EL usuario existe pero la contraseña es incorrecta");
             }
@@ -141,7 +175,25 @@ public class menuLogin extends javax.swing.JFrame {
             
         }
         
+        
     }//GEN-LAST:event_iniciarSesionActionPerformed
+
+    private void botonRegistroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonRegistroActionPerformed
+        // TODO add your handling code here:
+        agregarCliente nuevoCliente = new agregarCliente();
+        nuevoCliente.setVisible(true);
+        nuevoCliente.setLocationRelativeTo(null);
+    }//GEN-LAST:event_botonRegistroActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        bus newbus=new bus();
+        String[]asientos={"1","0","0","0","0","0","0","0","0","0","null"};
+        newbus.setAsientosOcupados(asientos);
+        busGUI bgui=new busGUI(newbus);
+        bgui.setVisible(true);
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -150,10 +202,12 @@ public class menuLogin extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel ImagenFondoLogin;
+    private javax.swing.JButton botonRegistro;
     private javax.swing.JButton botonSalir;
     private javax.swing.JPasswordField contraseñaTextField;
     private javax.swing.JLabel imagenLogin;
     private javax.swing.JButton iniciarSesion;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JTextField usuarioTextField;
