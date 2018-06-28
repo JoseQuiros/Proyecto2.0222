850cc9e69ed55f798d4e8ae3ca56bc8379bcbde7
JassonRomero <jassonromero18@gmail.com>
2018-06-28 14:55:47 -0600

    cambios en ventanas, validacion

--- src/GUI/Gui.java
+++ src/GUI/Gui.java
@@ -32,7 +32,7 @@ public class Gui extends javax.swing.JFrame {
 
     public Gui() throws IOException {
         initComponents();
-        
+
         modelo = new DefaultTableModel();
         tabla.setModel(modelo);
         // se añaden las columnas al Table
@@ -46,60 +46,58 @@ public class Gui extends javax.swing.JFrame {
 
         modelo.addColumn("Telefono");
 
-
         if (!file.exists()) {
 
+            try {
+                if (!file.exists()) {//si no esta el archivo se crea
 
-     
-        try{
-        if (!file.exists()) {//si no esta el archivo se crea
+                    file.createNewFile();
+                }
+                String ruta = "colaborador.txt";
+                File file = new File(ruta);
+                String cadena = "";
+                FileReader fr = new FileReader("colaborador.txt");
+                int lNumeroLineas = 0;
 
-            file.createNewFile();
-        }
-        String ruta = "colaborador.txt";
-        File file = new File(ruta);
-        String cadena = "";
-        FileReader fr = new FileReader("colaborador.txt");
-        int lNumeroLineas = 0;
-        
-        BufferedReader bf = new BufferedReader(fr);
-        while ((cadena = bf.readLine()) != null) {
-            lNumeroLineas++;
-        }
-        bf.close();
-        Object[] data = new Object[lNumeroLineas];
+                BufferedReader bf = new BufferedReader(fr);
+                while ((cadena = bf.readLine()) != null) {
+                    lNumeroLineas++;
+                }
+                bf.close();
+                Object[] data = new Object[lNumeroLineas];
 
-        cadena = "";
-        FileReader fileR = new FileReader(file);
-        BufferedReader buffReader = new BufferedReader(fileR);
+                cadena = "";
+                FileReader fileR = new FileReader(file);
+                BufferedReader buffReader = new BufferedReader(fileR);
 
-        int cantidadFilas = 0;
+                int cantidadFilas = 0;
 
-        //llenamos el Jtable
-        while ((cadena = buffReader.readLine()) != null) {
-            if (cadena.indexOf(",") != -1) {
+                //llenamos el Jtable
+                while ((cadena = buffReader.readLine()) != null) {
+                    if (cadena.indexOf(",") != -1) {
 
-                Object[] object = new Object[6];
+                        Object[] object = new Object[6];
 
-                object[0] = cadena.split(",")[0];
+                        object[0] = cadena.split(",")[0];
 
-                object[1] = cadena.split(",")[1];
+                        object[1] = cadena.split(",")[1];
 
-                object[2] = cadena.split(",")[2];
+                        object[2] = cadena.split(",")[2];
 
-                object[3] = cadena.split(",")[3];
+                        object[3] = cadena.split(",")[3];
 
-                object[4] = cadena.split(",")[4];
+                        object[4] = cadena.split(",")[4];
 
-                modelo.addRow(object);
+                        modelo.addRow(object);
+                    }
+                    //System.out.println(cadena);
+                    cantidadFilas++;
+                }
+                buffReader.close();
+                fileR.close();
+            } catch (IOException ex) {
+                System.out.println("upps algo salio mal" + ex);
             }
-            //System.out.println(cadena);
-            cantidadFilas++;
-        }
-        buffReader.close();
-        fileR.close();
-        }catch(IOException ex){
-            System.out.println("upps algo salio mal"+ex);
         }
     }
 
@@ -123,6 +121,19 @@ public class Gui extends javax.swing.JFrame {
         tabla = new javax.swing.JTable();
         jButtonEliminar = new javax.swing.JButton();
         jButton1_salir = new javax.swing.JButton();
+        jButtonEliminar1 = new javax.swing.JButton();
+        jButton1_salir1 = new javax.swing.JButton();
+        jTextFieldSNombre1 = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jTextFieldPUsuario1 = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
+        jComboBoxSexo1 = new javax.swing.JComboBox();
+        jLabel10 = new javax.swing.JLabel();
+        jTextFieldTelefono1 = new javax.swing.JTextField();
+        jLabel11 = new javax.swing.JLabel();
+        jTextFieldContraseña1 = new javax.swing.JTextField();
+        jButtonGuardar1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(102, 102, 102));
@@ -154,6 +165,11 @@ public class Gui extends javax.swing.JFrame {
         });
 
         jTextFieldSNombre.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        jTextFieldSNombre.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                jTextFieldSNombreKeyTyped1(evt);
+            }
+        });
 
         jLabel2.setBackground(new java.awt.Color(255, 255, 255));
         jLabel2.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
@@ -203,61 +219,6 @@ public class Gui extends javax.swing.JFrame {
             }
         });
 
-
-        jTextFieldSNombre.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyTyped(java.awt.event.KeyEvent evt) {
-                jTextFieldSNombreKeyTyped(evt);
-            }
-        });
-
-        jLabel2.setBackground(new java.awt.Color(255, 255, 255));
-        jLabel2.setText("Nombre");
-        jLabel2.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-
-        jLabel3.setText("Usuario");
-        jLabel3.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-
-        jTextFieldPUsuario.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyTyped(java.awt.event.KeyEvent evt) {
-                jTextFieldPUsuarioKeyTyped(evt);
-            }
-        });
-
-        jLabel6.setText("Sexo");
-        jLabel6.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-
-        jComboBoxSexo.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Masculino", "Femenino" }));
-
-        jLabel8.setText("Telefono");
-        jLabel8.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-
-        jTextFieldTelefono.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyTyped(java.awt.event.KeyEvent evt) {
-                jTextFieldTelefonoKeyTyped(evt);
-            }
-        });
-
-        jLabel9.setText("Contraseña");
-        jLabel9.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-
-        jTextFieldContraseña.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextFieldContraseñaActionPerformed(evt);
-            }
-        });
-        jTextFieldContraseña.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyTyped(java.awt.event.KeyEvent evt) {
-                jTextFieldContraseñaKeyTyped(evt);
-            }
-        });
-
-        jButtonGuardar.setText("Guardar");
-        jButtonGuardar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButtonGuardarActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -291,7 +252,7 @@ public class Gui extends javax.swing.JFrame {
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jButtonGuardar, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(105, 105, 105)))
+                        .addGap(104, 104, 104)))
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 486, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
@@ -326,18 +287,83 @@ public class Gui extends javax.swing.JFrame {
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jTextFieldContraseña, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel9, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButtonGuardar, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(29, 29, 29))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 253, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)))
+                        .addGap(18, 18, 18)
+                        .addComponent(jButtonGuardar, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 253, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jButton1_salir)
                     .addComponent(jButtonEliminar))
                 .addContainerGap(20, Short.MAX_VALUE))
         );
 
+        jButtonEliminar1.setText("Eliminar");
+        jButtonEliminar1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonEliminarActionPerformed(evt);
+            }
+        });
+
+        jButton1_salir1.setText("Salir");
+        jButton1_salir1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1_salirActionPerformed(evt);
+            }
+        });
+
+        jTextFieldSNombre1.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                jTextFieldSNombreKeyTyped(evt);
+            }
+        });
+
+        jLabel4.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("Nombre");
+        jLabel4.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        jLabel5.setText("Usuario");
+        jLabel5.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        jTextFieldPUsuario1.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                jTextFieldPUsuarioKeyTyped(evt);
+            }
+        });
+
+        jLabel7.setText("Sexo");
+        jLabel7.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        jComboBoxSexo1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Masculino", "Femenino" }));
+
+        jLabel10.setText("Telefono");
+        jLabel10.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        jTextFieldTelefono1.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                jTextFieldTelefonoKeyTyped(evt);
+            }
+        });
+
+        jLabel11.setText("Contraseña");
+        jLabel11.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        jTextFieldContraseña1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextFieldContraseñaActionPerformed(evt);
+            }
+        });
+        jTextFieldContraseña1.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                jTextFieldContraseñaKeyTyped(evt);
+            }
+        });
+
+        jButtonGuardar1.setText("Guardar");
+        jButtonGuardar1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonGuardarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -349,7 +375,10 @@ public class Gui extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         pack();
@@ -482,6 +511,10 @@ public class Gui extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jTextFieldContraseñaKeyTyped
 
+    private void jTextFieldSNombreKeyTyped1(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextFieldSNombreKeyTyped1
+
+    }//GEN-LAST:event_jTextFieldSNombreKeyTyped1
+
     private void limpiar() {
 
         jTextFieldPUsuario.setText("");
@@ -511,20 +544,33 @@ public class Gui extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1_salir;
+    private javax.swing.JButton jButton1_salir1;
     private javax.swing.JButton jButtonEliminar;
+    private javax.swing.JButton jButtonEliminar1;
     private javax.swing.JButton jButtonGuardar;
+    private javax.swing.JButton jButtonGuardar1;
     private javax.swing.JComboBox jComboBoxSexo;
+    private javax.swing.JComboBox jComboBoxSexo1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField jTextFieldContraseña;
+    private javax.swing.JTextField jTextFieldContraseña1;
     private javax.swing.JTextField jTextFieldPUsuario;
+    private javax.swing.JTextField jTextFieldPUsuario1;
     private javax.swing.JTextField jTextFieldSNombre;
+    private javax.swing.JTextField jTextFieldSNombre1;
     private javax.swing.JTextField jTextFieldTelefono;
+    private javax.swing.JTextField jTextFieldTelefono1;
     private javax.swing.JTable tabla;
     // End of variables declaration//GEN-END:variables
 }
